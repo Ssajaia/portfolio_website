e95b584155a53fd223d2e05eec94e4cd21a81366
S4B4 <120949625+Ssajaia@users.noreply.github.com>
2025-07-06 22:50:04 +0400

    add skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,6 +15,7 @@ const skills = [
   { name: "C++", level: 100 },
   { name: "Cyber security", level: 100 },
   { name: "ASP.NET", level: 20 },
+  { name: "Python", level:30},
 ];
 
 const Skills = () => {
