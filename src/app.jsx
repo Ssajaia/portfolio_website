a111c7f9f4df2f9f15751bb2f17b8cd3f39e2278
S4B4 <120949625+Ssajaia@users.noreply.github.com>
2026-01-16 21:00:31 +0400

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -35,9 +35,7 @@ function App() {
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
+    if (section) section.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
